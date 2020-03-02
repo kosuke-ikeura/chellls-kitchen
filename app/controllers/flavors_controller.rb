eeb5f31013f0673934bcd36d9bfb096874403f85
EC2 Default User <ec2-user@ip-172-31-20-174.ap-southeast-1.compute.internal>
2020-03-02 08:14:14 +0000

    before edit modal

--- app/controllers/flavors_controller.rb
+++ app/controllers/flavors_controller.rb
@@ -1,9 +1,23 @@
 class FlavorsController < ApplicationController
-    def index
-      @flavors = Flavor.where(id:1..25).order(id: "ASC").page(params[:page]).per(10)
-      @StatusImage = ["", "urn_red.png", "urn_yellow.png", "urn_blue.png"]
-    end
-    
-    def edit
-    end
+  before_action :authenticate_user!
+  def index
+    @flavors = Flavor.where(id:1..25).order(id: "ASC").page(params[:page]).per(10)
+    @StatusImage = ["", "urn_red.png", "urn_yellow.png", "urn_blue.png"]
+  end
+  def new
+    @flavor = Flavor.new
+  end
+  def edit
+    @flavor = Flavor.find(params[:id])
+  end
+  def update
+    @flavor = Flavor.find(params[:id])
+    @flavor.update(flavor_params)
+    redirect_to flavors_path
+  end
+  
+  private
+  def flavor_params
+    params.require(:flavor).permit(:name, :purchase_price, :status, :summit_price, :image).merge(user_id: current_user.id)
+  end
 end
